2da51600b23568eeaaad412970ffe1b154f4efdd
TrivikrammKY <122347637+TrivikrammKY@users.noreply.github.com>
2023-05-29 12:57:07 +0530

    Update MyAwesomeSpringBootAngularJsApp.java

--- src/main/java/com/tcs/angularjs/MyAwesomeSpringBootAngularJsApp.java
+++ src/main/java/com/tcs/angularjs/MyAwesomeSpringBootAngularJsApp.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyAwesomeSpringBootAngularJsApp {
+public class SudheerSpringBootAngularJsApp {
 
     public static void main(String[] args) {
         SpringApplication.run(SudheerSpringBootAngularJsApp.class, args);
